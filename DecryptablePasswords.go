b35cacb0e1252387344d1d10709e4d99f3a05f0f
Thomas Bouder <tbouder@protonmail.com>
2019-09-26 13:17:39 +0200

    Add Decryptablepasswordsg

---
+++ DecryptablePasswords.go
@@ -0,0 +1,80 @@
+/*******************************************************************************
+** @Author:					Thomas Bouder <Tbouder>
+** @Email:					Tbouder@protonmail.com
+** @Date:					Thursday 26 September 2019 - 13:09:47
+** @Filename:				DecryptablePasswords.go
+**
+** @Last modified by:		Tbouder
+** @Last modified time:		Thursday 26 September 2019 - 13:17:17
+*******************************************************************************/
+
+package		crypto
+
+import		"encoding/base64"
+import		"github.com/funny/crypto/aes256cbc"
+import		"github.com/microgolang/errors"
+
+/*EncryptPassword *************************************************************
+*	Encrypt a password according to the passPhrase and the token
+******************************************************************************/
+func	EncryptPassword(uncrypted, token, prependedMasterKey, appendedMasterKey string) (string, error) {
+	if (uncrypted == `` || token == `` || prependedMasterKey == `` || appendedMasterKey == ``) {
+		return ``, errors.New(`No encrypted, token or masterKeyPart`)
+	}
+	passPhrase := prependedMasterKey + token + appendedMasterKey
+	enc, err := aes256cbc.EncryptString(passPhrase, uncrypted)
+	if (errors.HasError(err)) {
+		return ``, err
+	}
+	return enc, nil
+}
+
+/*DecryptPassword *************************************************************
+*	Decrypt a password according to the passPhrase and the token
+******************************************************************************/
+func	DecryptPassword(crypted, token, prependedMasterKey, appendedMasterKey string) (string, error) {
+	if (crypted == `` || token == `` || prependedMasterKey == `` || appendedMasterKey == ``) {
+		return ``, errors.New(`No crypted, token or masterKeyPart`)
+	}
+	passPhrase := prependedMasterKey + token + appendedMasterKey
+	dec, err := aes256cbc.DecryptString(passPhrase, crypted);
+	if (errors.HasError(err)) {
+		return ``, err
+	}
+	return dec, nil
+}
+
+/*EncryptBusinessesDbPassword *************************************************
+*	Encrypt a password according to the passPhrase and the token
+******************************************************************************/
+func	EncryptPasswordWithBase64(uncrypted, token, prependedMasterKey, appendedMasterKey string) (string, error) {
+	if (uncrypted == `` || token == `` || prependedMasterKey == `` || appendedMasterKey == ``) {
+		return ``, errors.New(`No encrypted, token or masterKeyPart`)
+	}
+
+	passPhrase := prependedMasterKey+ token + appendedMasterKey
+	enc, err := aes256cbc.EncryptString(passPhrase, uncrypted);
+	if (errors.HasError(err)) {
+		return ``, err
+	}
+    data := base64.URLEncoding.EncodeToString([]byte(enc))
+	return string(data), nil
+}
+
+
+/*DecryptBusinessesDbPassword *************************************************
+*	Decrypt a password according to the passPhrase and the token
+******************************************************************************/
+func	DecryptPasswordWithBase64(crypted, token, prependedMasterKey, appendedMasterKey string) (string, error) {
+	if (crypted == `` || token == `` || prependedMasterKey == `` || appendedMasterKey == ``) {
+		return ``, errors.New(`No crypted, token or masterKeyPart`)
+	}
+
+	passPhrase := prependedMasterKey + token + appendedMasterKey
+    data, _ := base64.StdEncoding.DecodeString(crypted)
+	dec, err := aes256cbc.DecryptString(passPhrase, string(data));
+	if (errors.HasError(err)) {
+		return ``, err
+	}
+	return dec, nil
+}
